d4138978e4317ea9a07e2f9b8349a53cc937853a
Julian <20202067@student.hhs.nl>
2024-05-09 17:02:59 +0200

    comments

--- RGBLeds.cpp
+++ RGBLeds.cpp
@@ -9,7 +9,7 @@ RGBLeds::RGBLeds() : leds(AANTAL_LEDS, PIN, NEO_GRB + NEO_KHZ800) {
 void RGBLeds::brandlichtAan() {
     for(int i = 0; i <= AANTAL_LEDS - GROEP; i++) {
         for(int j = 0; j < GROEP; j++) {
-            leds.setPixelColor(i + j, leds.Color(255, 0, 0)); // Set pixel i+j to red
+            leds.setPixelColor(i + j, leds.Color(255, 0, 0));
         }
         leds.show();
         delay(20);
--- RGBLeds.ino
+++ RGBLeds.ino
@@ -1,12 +1,12 @@
 #include "RGBLeds.h"
 
-RGBLeds strip; // Maak een object van de klasse RGBController
+RGBLeds strip; // gebruikt pin D6 op de wemos
 
 void setup() {
 }
 
 void loop() {
-    strip.entreelichtAan(); // Roep de brandlichtRGB-functie aan
+    strip.entreelichtAan(); 
     delay(5000);
     for(int i = 0; i < 3; i++){
       strip.brandlichtAan();
